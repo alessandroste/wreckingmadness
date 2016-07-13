331da1fb48fada148575b8dff5b6977a5df38750
BuildTools <unconfigured@null.spigotmc.org>
2016-07-13 12:53:29 +0200

    Added android project

---
+++ proj.android/.d
@@ -0,0 +1,4 @@
+/media/dsk500-2/Cocos2D/wreckingmadness_prebuilt/proj.android/gen/com/enhance/gameservice/IGameTuningService.java : \
+  /media/dsk500-2/Cocos2D/wreckingmadness_prebuilt/proj.android/src/com/enhance/gameservice/IGameTuningService.aidl
+
+/media/dsk500-2/Cocos2D/wreckingmadness_prebuilt/proj.android/src/com/enhance/gameservice/IGameTuningService.aidl :
---
+++ proj.android/libs/gps/bin/AndroidManifest.xml.d
@@ -0,0 +1,2 @@
+/media/dsk500-2/Cocos2D/wreckingmadness_prebuilt/proj.android/libs/gps/bin/AndroidManifest.xml : \
+/media/dsk500-2/Cocos2D/wreckingmadness_prebuilt/proj.android/libs/gps/AndroidManifest.xml \
